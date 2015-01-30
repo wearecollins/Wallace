6cfe3cd607252e8dc6515c4c82991385ffb0febc
lars berg <laserstorm@gmail.com>
2015-01-29 20:33:44 -0600

    reset timing

--- html_v2/js/main.js
+++ html_v2/js/main.js
@@ -259,8 +259,8 @@ function APP( _useStats, _debug, _muteVideo, _auto)
 	 */
 
 	var rate = 2;
-	var backgroundTime = {start:6.386, end: 191.124};
-	// var backgroundTime = {start:162.386, end: 191.124};
+	// var backgroundTime = {start:6.386, end: 9.124};
+	var backgroundTime = {start:162.386, end: 191.124};
 	var bTransitioningBackground = false;
 	var done = false;
 
