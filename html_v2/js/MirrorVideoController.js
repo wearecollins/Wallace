278f0de5bbdfd7147f9eb1c6dcb94cca7d7c4c09
Brett Renfer <brett@robotconscience.com>
2015-02-03 12:14:37 -0500

    Now using videos hosted on Google Cloud Storage

--- html_v2/js/MirrorVideoController.js
+++ html_v2/js/MirrorVideoController.js
@@ -79,13 +79,22 @@ AB_1_Up.mp4
 		};
 	} else if ( !this.doubleWide ){
 		this.videoFiles = params.videoFiles || {
-			"BackgroundVideo": {path: "../WALLACE_TESTS/BG_PREVIEW_07_1" + fmt},
-			"01": {path: 	"../720p/AB_1_Straight_1" + fmt},
-			"02": {path: 	"../720p/AB_1_Up" + fmt},	
-			"03": {path: 	"../720p/AB_1_Down_1_1" + fmt},
-			"04": {path: 	"../720p/AB_1_Left_1_1_1" + fmt},
-			"05": {path: 	"../720p/AB_1_Right_1" + fmt},
+			"BackgroundVideo": {path: "http://storage.googleapis.com/wallace_videos/AB_BACKGROUND" + fmt},
+			"01": {path: 	"http://storage.googleapis.com/wallace_videos/AB_1_Straight_1_1" + fmt},
+			"02": {path: 	"http://storage.googleapis.com/wallace_videos/AB_1_Up" + fmt},	
+			"03": {path: 	"http://storage.googleapis.com/wallace_videos/AB_1_Down_1" + fmt},
+			"04": {path: 	"http://storage.googleapis.com/wallace_videos/AB_1_Left_1_2" + fmt},
+			"05": {path: 	"http://storage.googleapis.com/wallace_videos/AB_1_Right" + fmt},
 		};
+
+		/*
+		
+		"01": {path: 	"http://storage.googleapis.com/wallace_videos/AB_1_Straight_1_1" + fmt},
+			"02": {path: 	"http://storage.googleapis.com/wallace_videos/AB_1_Up" + fmt},	
+			"03": {path: 	"http://storage.googleapis.com/wallace_videos/AB_1_Down_1" + fmt},
+			"04": {path: 	"http://storage.googleapis.com/wallace_videos/AB_1_Left_1_2" + fmt},
+			"05": {path: 	"http://storage.googleapis.com/wallace_videos/AB_1_Right" + fmt},
+		 */
 	} else {
 		this.videoFiles = params.videoFiles || {
 			"BackgroundVideo": {path: "../WALLACE_TESTS/BG_PREVIEW_07_1" + fmt},
@@ -353,7 +362,7 @@ MirrorVideoController.prototype.loadVideos = function (fmt)
 	// var files = [];
 	for( var id in this.videoFiles )
 	{
-		this.loadVideo( id, this.videoFiles[id].path, fmt == "mp4" ? "video/mp4" : "video/webm");
+		this.loadVideo( id, this.videoFiles[id].path, "video/mp4" );//fmt == "mp4" ? "video/mp4" : "video/webm");
 		//this.videoToLoadCount++;
 	}
 	// videoLoader.addFiles(files);
@@ -412,6 +421,8 @@ MirrorVideoController.prototype.loadVideo = function ( name, url, type, onLoadCo
 
 	videoElement.setAttribute("id", name);
 	var source = document.createElement('source');
+	source.setAttribute('crossorigin', 'anonymous');
+	videoElement.setAttribute('crossorigin', 'anonymous');
 	source.src = url;
 	videoElement.load();
 	videoElement.appendChild(source);
